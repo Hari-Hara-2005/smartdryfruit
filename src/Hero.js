11856c88eace2a218fe405e4013f2c297457df0e
Hari-Hara-2005 <gharanthasec@gmail.com>
2024-07-19 20:55:14 +0530

    change hero section

--- src/Hero.js
+++ src/Hero.js
@@ -19,9 +19,9 @@ const Hero = () => {
                     src='/Images/leaf.png'
                     alt='leaf'
                     sx={{
-                        width: "12.5%",
+                        width: ["20%", "15%", "12.5%"],
                         ml: "3.5%",
-                        mt: "6.5%",
+                        mt: ["40%", "30%", "6.5%"],
                         transition: 'tranaform',
                         transition: 'all cubic-bezier(0.19, 1, 0.22, 1) 2.5s',
                         position: 'absolute',
@@ -33,7 +33,8 @@ const Hero = () => {
                         src='/Images/img-4.png'
                         alt='pack'
                         sx={{
-                            width: "28%",
+                            width: ["43%", "43%", "28%"],
+                            mt: [25, 30, 0],
                             transition: 'all cubic-bezier(0.19, 1, 0.22, 1) 2.5s',
                             position: 'absolute',
                             zIndex: 2
@@ -44,8 +45,8 @@ const Hero = () => {
                         src='/Images/cashew.png'
                         alt='pack'
                         sx={{
-                            width: "15%",
-                            mt: '26%',
+                            width: ["30%", "25%", "15%"],
+                            mt: ["90%", "70%", '26%'],
                             ml: '15%',
                             transition: 'all cubic-bezier(0.19, 1, 0.22, 1) 2.5s',
                             position: 'absolute',
@@ -58,7 +59,7 @@ const Hero = () => {
                     src='/Images/leaf2.png'
                     alt='leaf'
                     sx={{
-                        width: "14%",
+                        width: ["25%", "20%", "14%"],
                         bottom: 20,
                         right: "10rem",
                         transition: 'tranaform',
@@ -69,16 +70,16 @@ const Hero = () => {
             </Box>
             <Box sx={{ height: '102vh', bgcolor: '#92553D' }} className="two">
                 <Navbar />
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: "space-between", height: '80%', mx: 11 }}>
-                    <Box>
-                        <Box component='img' src='/Images/platte.png' alt='Cover 2' sx={{ width: "38%", position: 'absolute' }} />
+                <Box sx={{ display: 'flex', flexDirection: ['column', 'column', 'row'], alignItems: 'center', justifyContent: "space-between", height: '80%', mx: [2, 11] }}>
+                    <Box sx={{ display: 'flex', justifyContent: ['center', 'center', 'normal'] }}>
+                        <Box component='img' src='/Images/platte.png' alt='Cover 2' sx={{ width: ["60%", "60%","38%"], mt: [33, 50, 0], position: 'absolute'}} />
                     </Box>
-                    <Box sx={{ width: "50%" }}>
+                    <Box sx={{ width: ["100%", "100%", "50%"] }}>
                         <Typography sx={{
                             fontSize: ['1.5rem', '1.5rem', '2.5rem'],
                             fontWeight: '600',
                             color: 'white',
-                            py:1
+                            py: 1
                         }}>
                             Flavour Updated
                         </Typography>
--- src/gsapAnimations.js
+++ src/gsapAnimations.js
@@ -4,22 +4,62 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export const animateHero = () => {
-    // First timeline
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".two",
-            start: "0% 100%",
-            end: "70% 50%",
-            scrub: true,
-            // markers: true,
-        }
+    // Create a matchMedia instance
+    const mm = gsap.matchMedia();
+
+    mm.add("(min-width: 769px)", () => {
+        // Animations for desktop
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".two",
+                start: "0% 100%",
+                end: "70% 50%",
+                scrub: true,
+                // markers: true,
+            }
+        });
+
+        tl.to("#pack", { top: "115%", left: "10%" }, 'orange')
+            .to("#cashew", { width: "15%", top: "125%", right: "10%" }, 'orange')
+            .to("#leaf", { top: "110%", rotate: "65deg", left: "70%" }, 'orange')
+            .to("#leaf2", { top: "120%", rotate: "200deg", left: "0%" }, 'orange');
     });
 
-    tl.to("#pack", { top: "115%", left: "10%" }, 'orange')
-        // .to("#orange-cut", { top: "160%", left: "23%" }, 'orange')
-        .to("#cashew", { width: "15%", top: "125%", right: "10%" }, 'orange')
-        .to("#leaf", { top: "110%", rotate: "65deg", left: "70%" }, 'orange')
-        .to("#leaf2", { top: "120%", rotate: "200deg", left: "0%" }, 'orange');
+    mm.add("(max-width: 768px)", () => {
+        // Animations for tablets and below
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".two",
+                start: "0% 100%",
+                end: "70% 50%",
+                scrub: true,
+                // markers: true,
+            }
+        });
+
+        tl.to("#pack", { top: "0%", left: "50%" }, 'orange')
+            .to("#cashew", { width: "30%", top: "130%", right: "5%" }, 'orange')
+            .to("#leaf", { top: "115%", rotate: "45deg", left: "60%" }, 'orange')
+            .to("#leaf2", { top: "110%", rotate: "180deg", left: "5%" }, 'orange');
+    });
+
+    mm.add("(max-width: 480px)", () => {
+        // Animations for mobile devices
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".two",
+                start: "0% 100%",
+                end: "70% 50%",
+                scrub: true,
+                // markers: true,
+            }
+        });
+
+        tl.to("#pack", { top: "110%", left: "28%" }, 'orange')
+            .to("#cashew", { width: "25%", top: "140%", right: "0%" }, 'orange')
+            .to("#leaf", { top: "120%", rotate: "30deg", left: "70%" }, 'orange')
+            .to("#leaf2", { top: "120%", rotate: "150deg", left: "8%" }, 'orange');
+    });
 
     // Second timeline
     // const tl2 = gsap.timeline({
