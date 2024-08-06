bf1c6d529eaa43a72695b96322430b534e786bb2
Hari-Hara-2005 <gharanthasec@gmail.com>
2024-08-07 00:32:44 +0530

    change about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -24,6 +24,7 @@ export const About = () => {
         lockRef.current?.playFromBeginning();
         truckRef.current?.playFromBeginning();
         qualityRef.current?.playFromBeginning();
+        
         coinsRef.current?.playFromBeginning();
     }, []);
 
